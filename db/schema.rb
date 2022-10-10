d5c97caa26d8e8ff3ffe17f5f32fc28a0fa458c1
Mike Rachman <mikerachman1@gmail.com>
2022-10-10 11:57:15 -0400

    created task and list models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_154927) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_155529) do
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email", default: "", null: false
@@ -24,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_154927) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lists", "users"
+  add_foreign_key "tasks", "lists"
 end
