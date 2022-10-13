b38b0e04bdeab463e4be9b29e1daafd3164ab036
Mike Rachman <mikerachman1@gmail.com>
2022-10-13 10:55:19 -0400

    added username field to devise signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable  
   
-  has_many :lists
+  has_many :lists, dependent: :destroy
 
   validates :username, presence: true
 end
