1e6ebb19235785cf68b5b91de41a5a270802664a
Mike Rachman <mikerachman1@gmail.com>
2022-10-15 11:28:07 -0400

    added edit link to tasks on list show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,18 @@ class TasksController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def edit
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+  end 
+
+  def update
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    @task.update(task_params)
+    redirect_to list_path(@list)
+  end
+
   def destroy
     @list = List.find(params[:list_id])
     @task = @list.tasks.find(params[:id])
